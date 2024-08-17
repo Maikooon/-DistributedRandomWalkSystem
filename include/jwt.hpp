898f5ff7ac863887a1373cbb62598e31dca0cb78
kate0626 <101503385+Maikooon@users.noreply.github.com>
2024-08-17 16:00:34 +0900

    challange jwt
    できないコーーど追加

--- include/jwt.hpp
+++ include/jwt.hpp
@@ -9,25 +9,40 @@
 std::string generateJWT(uint32_t RWer_id, const std::string &secret_key)
 {
     auto token = jwt::create()
-                     .set_payload_claim("id", jwt::claim(std::to_string(RWer_id)))
+                     .set_payload_claim("id", jwt::claim(std::to_string(6)))
                      .sign(jwt::algorithm::hs256{secret_key});
+    // auto token = jwt::create()
+    //                  .set_payload_claim("id", jwt::claim(std::to_string(RWer_id)))
+    //                 .sign(jwt::algorithm::hs256{secret_key});
+    // エンコードされたトークンを出力
+    std::cout << "Generated JWT: " << token << std::endl;
+    std::cout << "Token size: " << token.size() << std::endl;
     return token;
 }
 
 bool verifyJWT(const std::string &token, const std::string &secret_key, uint32_t &RWer_id)
 {
-    // とりあえず実行されたら文字列を表示する
-    std::cout << "aaaaaaaaaaa" << std::endl;
+    // 固定されたトークン文字列を使用する
+    // const std::string fixed_token = "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IjYifQ.Wx11kP8GMJeUhnV4Th9t2tfCZyAIeidP1rcFiv_c_pY";
+
+    // // 無理やりトークンのサイズを79バイトに合わせる
+    // std::string adjusted_token = token.substr(0, 79);
+
+    // トークンを受け取った時の状態を出力
+    std::cout << "Received JWT: " << token << std::endl;
+    std::cout << "Token size: " << token.size() << std::endl;
+
     try
     {
         auto decoded = jwt::decode(token);
         auto verifier = jwt::verify()
                             .allow_algorithm(jwt::algorithm::hs256{secret_key})
-                            .with_claim("id", jwt::claim(std::string{}));
+                            .with_claim("id", jwt::claim(std::to_string(6))); // 検証対象のRWer_idを指定
 
         verifier.verify(decoded);
-        std::cout << "Extracted RWer_id: " << RWer_id << std::endl;
+
         RWer_id = std::stoi(decoded.get_payload_claim("id").as_string());
+        std::cout << "Extracted RWer_id: " << RWer_id << std::endl;
         return true;
     }
     catch (const std::exception &e) // 一般的な例外クラス
